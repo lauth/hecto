ccc2581d374f3340ff3df94e2c1f9bc07cf9f263
chath <lauth@free.fr>
2021-03-14 20:03:15 +0100

    hecto: chapter 6 - search forward

--- src/editor.rs
+++ src/editor.rs
@@ -12,7 +12,7 @@ const STATUS_BG_COLOR: color::Rgb = color::Rgb(239, 239, 239);
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 const QUIT_TIMES: u8 = 3;
 
-#[derive(Default)]
+#[derive(Default, Clone)]
 pub struct Position {
     pub x: usize,
     pub y: usize,
@@ -106,7 +106,7 @@ impl Editor {
 
     fn save(&mut self) {
         if self.document.file_name.is_none() {
-            let new_name = self.prompt("Save as: ").unwrap_or(None);
+            let new_name = self.prompt("Save as: ", |_, _, _| {}).unwrap_or(None);
             if new_name.is_none() {
                 self.status_message = StatusMessage::from("Save aborted.".to_string());
                 return;
@@ -118,6 +118,39 @@ impl Editor {
             self.status_message = StatusMessage::from("File saved successfully.".to_string());
         } else {
             self.status_message = StatusMessage::from("Error writing file!".to_string());
+        }
+    }
+
+    fn search(&mut self) {
+        let old_position = self.cursor_position.clone();
+
+        if let Some(query) = self
+        .prompt("Search (ESC to cancel, arrows to navigate): ",
+        |editor, key, query| {
+            let mut moved = false;
+            match key {
+                Key::Right | Key::Down => {
+                    editor.move_cursor(Key::Right);
+                    moved = true;
+                },
+                _ => (),
+            }
+
+            if let Some(position) = editor.document.find(&query, &editor.cursor_position) {
+                editor.cursor_position = position;
+                editor.scroll();
+            } else if moved {
+                editor.move_cursor(Key::Left);
+            }
+        }).unwrap_or(None) {
+            if let Some(position) = self.document.find(&query[..], &old_position) {
+                self.cursor_position = position;
+            } else {
+                self.status_message = StatusMessage::from(format!("Not found: {}", query));
+            }
+        } else {
+            self.cursor_position = old_position;
+            self.scroll();
         }
     }
 
@@ -137,15 +170,7 @@ impl Editor {
                 self.should_quit = true
             },
             Key::Ctrl('s') => self.save(),
-            Key::Ctrl('f') => {
-                if let Some(query) = self.prompt("Search: ").unwrap_or(None) {
-                    if let Some(position) = self.document.find(&query[..]) {
-                        self.cursor_position = position;
-                    } else {
-                        self.status_message = StatusMessage::from(format!("Not found: {}", query));
-                    }
-                }
-            },
+            Key::Ctrl('f') => self.search(),
             Key::Char(c) => {
                 self.document.insert(&self.cursor_position, c);
                 self.move_cursor(Key::Right);
@@ -344,13 +369,17 @@ impl Editor {
         }
     }
 
-    fn prompt(&mut self, prompt: &str) -> Result<Option<String>, std::io::Error> {
+    fn prompt<C>(&mut self, prompt: &str, callback: C) -> Result<Option<String>, std::io::Error> 
+    where
+        C: Fn(&mut Self, Key, &String),
+    {
         let mut result = String::new();
 
         loop {
             self.status_message = StatusMessage::from(format!("{}{}", prompt, result));
             self.refresh_screen()?;
-            match Terminal::read_key()? {
+            let key = Terminal::read_key()?;
+            match key {
                 Key::Backspace => result.truncate(result.len().saturating_sub(1)),
                 Key::Char('\n') => break,
                 Key::Char(c) => {
@@ -364,6 +393,7 @@ impl Editor {
                 },
                 _ => (),
             }
+            callback(self, key, &result);
         }
         self.status_message = StatusMessage::from(String::new());
         if result.is_empty() {
